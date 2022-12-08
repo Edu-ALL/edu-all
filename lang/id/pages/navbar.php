69d8da5f6059cda312b8b0224f7e44ce4c8a3799
Niel Pasae <sjrnl27@gmail.com>
2022-12-08 23:50:26 +0700

    menambahkan home page & fix navbar

--- lang/id/pages/navbar.php
+++ lang/id/pages/navbar.php
@@ -1,22 +1,35 @@
 <?php
 
 return [
-    'navbar_home' => 'Beranda',
-    'navbar_programs' => 'Program Kami',
-    'navbar_programs_admission' => 'Admission Mentoring',
-    'navbar_programs_admission_undergraduate' => 'Undergraduate Program',
-    'navbar_programs_admission_graduate' => 'Graduate Program',
-    'navbar_programs_admission_university' => 'University Transfer',
-    'navbar_programs_passion' => 'Passion Project Mentoring',
-    'navbar_programs_academic' => 'Academic & Test Preparation',
-    'navbar_programs_academic_tutoring' => 'Academic Tutoring',
-    'navbar_programs_academic_ib' => 'IB Extended Essay Coaching Program',
-    'navbar_programs_academic_sat' => 'Persiapan SAT / ACT',
-    'navbar_about_us' => 'Tentang Kami',
-    'navbar_success_stories' => 'Cerita Sukses',
-    'navbar_upcoming_events' => 'Event Kami',
-    'navbar_partnership' => "Kemitraan dan Karir",
-    'navbar_blog' => "Blog",
-    'navbar_contact_us' => "Kontak Kami",
-    "navbar_guidebooks" => "Guidebooks Gratis",
+    'home' => 'Beranda',
+
+    // programs
+    'programs' => 'Program Kami',
+    'programs_admission' => 'Admission Mentoring',
+    'programs_admission_undergraduate' => 'Undergraduate Program',
+    'programs_admission_graduate' => 'Graduate Program',
+    'programs_admission_university' => 'University Transfer',
+    'programs_passion' => 'Passion Project Mentoring',
+    'programs_academic' => 'Academic & Test Preparation',
+    'programs_academic_tutoring' => 'Academic Tutoring',
+    'programs_academic_ib' => 'IB Extended Essay Coaching Program',
+    'programs_academic_sat' => 'Persiapan SAT / ACT',
+
+    // about us
+    'about_us' => 'Tentang Kami',
+    'partnership' => "Kemitraan dan Karir",
+
+    // blog
+    'blog' => "All-info Blog",
+
+    // resources
+    'resources' => "Resources",
+    'success_stories' => 'Cerita Sukses',
+    'upcoming_events' => 'Event Kami',
+    "guidebooks" => "Guidebooks Gratis",
+    'testimonial' => "Testimonial",
+    'student_acceptances' => "Student Acceptances",
+
+    // contact us
+    'contact_us' => "Kontak Kami",
 ];
