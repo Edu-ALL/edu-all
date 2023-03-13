2ca83364da37e2ae65329b189e0d98f567c65967
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-03-13 12:16:13 +0700

    13/03/2023

--- resources/views/sitemap/blog.blade.php
+++ resources/views/sitemap/blog.blade.php
@@ -1,4 +1,5 @@
-<?xml-stylesheet type="text/xsl" href="{{ asset('css/main-sitemap.xsl') }}"?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<?php echo '<?xml-stylesheet type="text/xsl" href="'.asset("css/main-sitemap.xsl").'"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($blogs as $blog)
         <url>
