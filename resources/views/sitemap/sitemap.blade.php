c20859dc5fe76abe8751da5d7f9267e8b4cf5203
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-03-13 14:29:09 +0700

    13/03/2023

--- resources/views/sitemap/sitemap.blade.php
+++ resources/views/sitemap/sitemap.blade.php
@@ -1,4 +1,5 @@
-<?xml-stylesheet type="text/xsl" href="{{asset('css/main-sitemap.xsl')}}"?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<?php echo '<?xml-stylesheet type="text/xsl" href="'.asset("css/main-sitemap.xsl").'"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         <url>
             <loc>{{ url('id-en/sitemap-pages.xml')}}</loc>
