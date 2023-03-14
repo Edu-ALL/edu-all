23afbdfc4854179dde320c79a8fb765ac3f8d834
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-03-14 15:46:26 +0700

    14/03/2023

--- lang/id/pages/resources/upcoming_events.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'title' => 'Upcoming Events',
-    'body' => 'Unduh Kalender Student-Friendly 2023 Kami',
-    'btn' => "Unduh",
-
-    'bottom_title' => 'Untuk info lebih lanjut dan pendaftaran',
-    'bottom_btn' => 'Kontak Kami'
-];
