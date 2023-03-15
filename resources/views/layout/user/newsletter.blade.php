31cad88d9949b70630999057e62455ebd4f7465d
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-03-15 19:02:36 +0700

    15/03/2023

---
+++ resources/views/layout/user/newsletter.blade.php
@@ -0,0 +1,130 @@
+<div id="mlb2-4031658" class="ml-form-embedContainer ml-subscribe-form ml-subscribe-form-4031658">
+    <div class="ml-form-align-center ">
+        <div class="ml-form-embedWrapper embedForm">
+
+            <div class="ml-form-embedBody ml-form-embedBodyHorizontal row-form">
+
+                <div class="ml-form-embedContent" style=" ">
+                    <h4></h4>
+                    <p><span style="font-size: 24px;">ALL-in Eduspace Newsletter</span><span
+                            style="font-size: 24px;"><br></span></p>
+                    <p><span style="font-size: 18px;">Signup for news and special offers!<br></span></p>
+                    <p><span style="font-size: 18px;"><strong></strong></span><span style="font-size: 18px;"><br></span>
+                    </p>
+                </div>
+
+                <form class="ml-block-form"
+                    action="https://assets.mailerlite.com/jsonp/253019/forms/82679999451105091/subscribe" data-code=""
+                    method="post" target="_blank">
+                    <div class="ml-form-formContent">
+
+                        <div class="ml-form-fieldRow ">
+                            <div class="ml-field-group ml-field-name ml-validate-required">
+
+                                <!-- input -->
+                                <input aria-label="name" aria-required="true" type="text" class="form-control"
+                                    data-inputmask="" name="fields[name]" placeholder="Name" autocomplete="name">
+                            </div>
+                        </div>
+                        <div class="ml-form-fieldRow ">
+                            <div class="ml-field-group ml-field-email ml-validate-email ml-validate-required">
+                                <!-- input -->
+                                <input aria-label="email" aria-required="true" type="email" class="form-control"
+                                    data-inputmask="" name="fields[email]" placeholder="Email" autocomplete="email">
+                            </div>
+                        </div>
+                        <div class="ml-form-fieldRow ">
+                            <div class="ml-field-group ml-field-audience ml-validate-required">
+
+                                <label>You are...</label>
+                                <!-- radio -->
+                                <div class="custom-checkbox-radio-list">
+                                    <!-- Visible if current or any next has value -->
+                                    <div class="custom-control custom-radio">
+                                        <input aria-label="audience" aria-required="true" name="fields[audience]"
+                                            class="custom-control-input" type="radio" value="Parents"
+                                            id="radio-4031658-23}-0">
+                                        <label class="custom-control-label" for="radio-4031658-23}-0">
+                                            Parents
+                                        </label>
+                                    </div>
+
+                                    <div class="custom-control custom-radio">
+                                        <input aria-label="audience" aria-required="true" name="fields[audience]"
+                                            class="custom-control-input" type="radio" value="Student"
+                                            id="radio-4031658-23}-1">
+                                        <label class="custom-control-label" for="radio-4031658-23}-1">
+                                            Student
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-form-fieldRow ml-last-item">
+                            <div class="ml-field-group ml-field-grade_waktu_ikut_program ml-validate-required">
+
+                                <label>Your goals..</label>
+                                <!-- radio -->
+                                <div class="custom-checkbox-radio-list">
+                                    <!-- Visible if current or any next has value -->
+                                    <div class="custom-control custom-radio">
+                                        <input aria-label="grade_waktu_ikut_program" aria-required="true"
+                                            name="fields[grade_waktu_ikut_program]" class="custom-control-input"
+                                            type="radio" value="Undergraduate Degree" id="radio-4031658-33}-0">
+                                        <label class="custom-control-label" for="radio-4031658-33}-0">
+                                            Undergraduate Degree
+                                        </label>
+                                    </div>
+
+                                    <div class="custom-control custom-radio">
+                                        <input aria-label="grade_waktu_ikut_program" aria-required="true"
+                                            name="fields[grade_waktu_ikut_program]" class="custom-control-input"
+                                            type="radio" value="Master Degree" id="radio-4031658-33}-1">
+                                        <label class="custom-control-label" for="radio-4031658-33}-1">
+                                            Master Degree
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <input type="hidden" name="ml-submit" value="1">
+
+                    <div class="ml-form-embedSubmit">
+
+                        <button type="submit" class="primary">Submit</button>
+
+                        <button disabled="disabled" style="display: none;" type="button" class="loading">
+                            <div class="ml-form-embedSubmitLoad"></div>
+                            <span class="sr-only">Loading...</span>
+                        </button>
+                    </div>
+                    <input type="hidden" name="anticsrf" value="true">
+                </form>
+            </div>
+
+            <div class="ml-form-successBody row-success" style="display: none">
+
+                <div class="ml-form-successContent">
+
+                    <h4>Thanks!</h4>
+
+                    <p style="text-align: center;">Our team will contact you very soon</p>
+                    <p><br></p>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    function ml_webform_success_4031658() {
+        var $ = ml_jQuery || jQuery;
+        $('.ml-subscribe-form-4031658 .row-success').show();
+        $('.ml-subscribe-form-4031658 .row-form').hide();
+    }
+</script>
+<script src="https://groot.mailerlite.com/js/w/webforms.min.js?v300817f630ad0e957914d0b28a2b6d78"
+    type="text/javascript"></script>
