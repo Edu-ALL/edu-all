87f25ee47697f417d2f94756d13e2ddb9038e14a
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-03-24 16:20:55 +0700

    24/03/2023

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\GzipMiddleware::class,
     ];
 
     /**
@@ -65,6 +64,5 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'setlocale' => \App\Http\Middleware\SetLocale::class,
         'is_admin' => \App\Http\Middleware\IsAdmin::class,
-        'gzip' => \App\Http\Middleware\GzipMiddleware::class,
     ];
 }
