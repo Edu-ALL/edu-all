7fb43f750c406ae74c87f4e209e91e7e645f9660
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-04-05 10:10:39 +0700

    05/04/2023 - banner

--- resources/views/layout/admin/app.blade.php
+++ resources/views/layout/admin/app.blade.php
@@ -88,6 +88,31 @@
         $("img").lazyload({
             effect: "fadeIn"
         });
+
+        function notif(status, message) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'bottom-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            // if (status == true) {
+            //     var icon = "success"
+            // } else {
+            //     var icon = "error"
+            // }
+
+            Toast.fire({
+                icon: status,
+                title: message
+            })
+        }
     </script>
 
     @yield('js')
