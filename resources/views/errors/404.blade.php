f22dd55e910f1061f65bec8f85a4359766f050be
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-04-06 10:02:25 +0700

    06/04/2023

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,14 +2,24 @@
 
 @section('content')
     <section class="main-container">
-        <div class="flex flex-col items-center justify-center max-w-2xl w-full  h-[60vh] mx-auto">
-            <h1 class="font-primary font-bold text-6xl text-primary text-center">Oops... Page Not Found!</h1>
-            <a href="{{ route('home', app()->getLocale()) }}" class="flex justify-center w-full pt-8">
-                <span
-                    class="block max-w-[200px] w-full px-4 py-2 rounded-md bg-yellow font-primary font-semibold text-base text-white text-center">
-                    Back Home
-                </span>
-            </a>
+        <div class="flex md:flex-row flex-col items-center justify-between max-w-5xl w-full  md:h-[70vh] h-auto mx-auto">
+            <div class="md:text-start text-center md:mt-0 mt-[40px]  w-50">
+                <h1 class="font-primary font-bold md:text-6xl text-4xl text-primary">Oops!</h1>
+                <h2 class="font-primary mt-5 md:text-3xl text-2xl font-normal">Error 404 - Page Not Found</h2>
+                <h2 class="font-primary mt-3 md:text-2xl text-xl font-normal">
+                    The page you requested could not be found. <br>
+                    We're working on it :)
+                </h2>
+                <a href="{{ route('home', app()->getLocale()) }}" class="flex md:justify-start justify-center w-full pt-8">
+                    <span
+                        class="block px-4 py-2 rounded-md bg-yellow font-primary font-semibold text-base text-white text-center">
+                      <i class="fa fa-arrow-left fa-fw"></i>  Back Home
+                    </span>
+                </a>
+            </div>
+            <div class="w-50">
+                <img src="{{asset('assets/img/404.png')}}" alt="404" class="w-[100%]" loading="lazy">
+            </div>
         </div>
     </section>
 @endsection
--- resources/views/user/sign_me/thank.blade.php
+++ resources/views/user/sign_me/thank.blade.php
@@ -3,14 +3,14 @@
 @section('content')
     <section class="main-container">
         <div class="flex flex-col items-center justify-center max-w-4xl w-full  h-[60vh] mx-auto">
-            <h1 class="font-primary font-bold text-4xl text-primary text-center">
+            <h1 class="font-primary font-bold md:text-3xl text-xl text-primary text-center">
                 Thank you for letting us know a little bit about you <i>Anggie</i> or <i>Derry</i> will contact you soon for
                 the further consultation
             </h1>
             <a href="{{ route('home', app()->getLocale()) }}" class="flex justify-center w-full pt-8">
                 <span
-                    class="block max-w-[200px] w-full px-4 py-2 rounded-md bg-yellow font-primary font-semibold text-base text-white text-center">
-                    Back Home
+                    class="block px-4 py-2 rounded-md bg-yellow font-primary font-semibold text-base text-white text-center">
+                    <i class="fa fa-arrow-left fa-fw"></i> Back Home
                 </span>
             </a>
         </div>
