b08f46985496c72ff9215ef216c39203fbe8f46c
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-05-04 12:05:53 +0700

    04/05/2023

--- resources/views/user/home/region/id.blade.php
+++ resources/views/user/home/region/id.blade.php
@@ -23,7 +23,7 @@
                             <div
                                 class="absolute left-0 top-0 flex items-center w-full h-full pt-16 md:items-start {{ !$banner->banner_img_mobile ? 'bg-gradient-to-r from-primary/90' : '' }}">
                                 <div
-                                    class="flex flex-col justify-center items-center w-full h-full max-w-5xl px-6 lg:px-40 2xl:px-44 font-primary lg:items-start">
+                                    class="flex flex-col justify-center items-center w-full h-full max-w-5xl px-6 lg:px-40 2xl:px-[220px] font-primary lg:items-start">
                                     <h1
                                         class="font-primary text-3xl font-bold text-white text-center mb-6 sm:text-4xl lg:text-5xl lg:text-left xl:text-6xl">
                                         {{ $banner->banner_title }}</h1>
@@ -31,7 +31,7 @@
                                         class="text-sm font-semibold text-white text-center sm:text-lg lg:text-lg lg:text-left xl:text-xl">
                                         {!! $banner->banner_description !!}
                                     </h2>
-                                    <a href="{{ $banner->banner_link }}" class="{{ $banner->banner_title == "" ? 'mt-[15vh]' : 'mt-0' }}">
+                                    <a href="{{ $banner->banner_link }}" class="{{ $banner->banner_title == "" ? 'md:mt-[25vh] mt-[20vh]' : 'mt-0' }}">
                                         <span
                                             class="inline-block mt-10 bg-primary py-2.5 px-8 rounded-lg font-bold text-white text-base capitalize"
                                             style="background-color: {{ $banner->banner_button_color }}">
