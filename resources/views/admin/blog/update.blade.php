a19695e16b1b2356fb7eb0cb738464e0f7d655fa
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-05-16 11:24:53 +0700

    16/5/2023 blog desc

--- resources/views/admin/blog/update.blade.php
+++ resources/views/admin/blog/update.blade.php
@@ -315,7 +315,7 @@
         height: '350',
         plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
         toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
-        paste_as_text: true,
+        paste_as_text: false,
         setup: function (ed) {
             ed.on("change", function () {
                 getDuration(ed); // function for duration minute in Blogs
