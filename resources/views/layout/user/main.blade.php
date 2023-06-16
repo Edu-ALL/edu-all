606013d4d3e9eff02afaab5e5eb699f140d49400
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-06-16 09:03:19 +0700

    16/06/2023

--- resources/views/layout/user/main.blade.php
+++ resources/views/layout/user/main.blade.php
@@ -145,7 +145,7 @@
 
     <div class="fixed lg:bottom-5 bottom-[15px] lg:right-5 right-[10px] z-[9999] transition-all duration-1000">
         <div class="relative group">
-            <div class="absolute bottom-[65px] right-0 invisible translate-x-[1px] translate-y-8 transition-all duration-500 opacity-0 group-hover:visible group-hover:translate-y-0 group-hover:opacity-100">
+            {{-- <div class="absolute bottom-[65px] right-0 invisible translate-x-[1px] translate-y-8 transition-all duration-500 opacity-0 group-hover:visible group-hover:translate-y-0 group-hover:opacity-100">
                 <ul class="shadow-lg rounded bg-gray-100">
                     <li class="p-2 border-b border-gray-500 hover:bg-primary hover:text-white">
                         <a href="https://api.whatsapp.com/send?phone=6281808081363&text=Hello%20ALL-in,%20I%20am%0AName%20:%0AGrade%20:%0ASchool%20:%0ADestination%20Country%20:%0AMajor%20:%0A%0A*I*%20*want*%20*to*%20*ask*%20*about...*"
@@ -164,11 +164,13 @@
                         </a>
                     </li>
                 </ul>
-            </div>
+            </div> --}}
     
             <div
                 class="absolute right-0 bottom-0 bg-[#008069] hover:bg-white rounded-full md:w-[60px] md:h-[60px] w-[50px] h-[50px] flex justify-center items-center text-white hover:text-[#008069] border-[1px] border-[#008069] cursor-pointer shadow" id='open-nav-child-btn'>
-                <i class="fa fa-whatsapp text-[30px]"></i>
+                <a href="https://api.whatsapp.com/send?phone=6287888827686&text=Hello%20ALL-in,%20I%20am%0AName%20:%0AGrade%20:%0ASchool%20:%0ADestination%20Country%20:%0AMajor%20:%0A%0A*I*%20*want*%20*to*%20*ask*%20*about...*">
+                    <i class="fa fa-whatsapp text-[30px]"></i>
+                </a>
             </div>
         </div>
     </div>
