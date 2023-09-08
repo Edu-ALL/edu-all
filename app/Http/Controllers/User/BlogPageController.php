be94c1adb8b1507b3f92b4d618567b098a630dbe
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-09-08 14:56:38 +0700

    update user blog detail

--- app/Http/Controllers/User/BlogPageController.php
+++ app/Http/Controllers/User/BlogPageController.php
@@ -104,7 +104,7 @@ class BlogPageController extends Controller
             // update blog click count
             $blog->update([
                 'click_count' => $blog->click_count + 1,
-                'updated_at' => $last_updated,
+                'timestamps' => false
             ]);
         }
 
