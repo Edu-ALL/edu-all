f2a29c11473aa4845ff43defeed0c31e5a6df863
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2024-08-13 11:03:54 +0700

    update fontawesome

--- resources/views/layout/admin/app.blade.php
+++ resources/views/layout/admin/app.blade.php
@@ -15,7 +15,7 @@
         rel="stylesheet">
 
     {{-- Font Awesome --}}
-    <script src="https://kit.fontawesome.com/d11faf3e43.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     {{-- JQuery --}}
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
