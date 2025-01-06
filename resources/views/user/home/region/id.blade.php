fd2398b890f16fb6a4cb7cd604cb1c67895718b3
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2025-01-06 11:38:30 +0700

    update home page

--- resources/views/user/home/region/id.blade.php
+++ resources/views/user/home/region/id.blade.php
@@ -26,16 +26,16 @@
                 </video>
             @elseif ($banners->image)
                 <div class="w-full md:h-[100vh] h-[100dvh] bg-black">
-                    {{-- <img loading="lazy"
+                    <img loading="lazy"
                         src="{{ asset('uploaded_files/banner/' . $banners->updated_at->format('Y') . '/' . $banners->updated_at->format('m') . '/' . $banners->image) }}"
-                        alt="{{ $banners->alt }}" class="w-full md:h-[100vh] h-[100dvh] object-cover absolute top-0 left-0"> --}}
+                        alt="{{ $banners->alt }}" class="w-full md:h-[100vh] h-[100dvh] object-cover absolute top-0 left-0">
                 </div>
             @endif
             <div class="absolute bottom-72 md:bottom-52 left-0 right-0 w-full main-container">
-                <h2
-                    class="font-bold text-xl md:text-[52px] text-white text-center lg:text-left max-w-lg mx-auto lg:mx-0 md:leading-[3.5rem]">
+                <p
+                    class="font-bold text-white text-center lg:text-left max-w-lg mx-auto lg:mx-0 md:leading-[3.5rem]">
                     Your Gateway to Top Universities & Dream Careers
-                </h2>
+                </p>
                 <x-registration-form :is-button="true" />
             </div>
             <div class="absolute md:bottom-10 left-0 right-0">
