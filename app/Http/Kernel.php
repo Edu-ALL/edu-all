a1b3fd14a99af4682ff9ae3b0381da5f149e0c84
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2025-09-02 08:15:06 +0700

    inactive logroute middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \App\Http\Middleware\RedirectWwwToNonWww::class,
-            \App\Http\Middleware\LogRouteChange::class,
+            // \App\Http\Middleware\LogRouteChange::class,
             // \App\Http\Middleware\SetCacheHeaders::class,
         ],
 
