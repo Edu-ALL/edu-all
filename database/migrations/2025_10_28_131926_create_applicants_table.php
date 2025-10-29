b505b9d4f3a0e7f507f1651dd96e231d85b2f7fe
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2025-10-29 11:55:56 +0700

    production-aws

--- database/migrations/2025_10_28_131926_create_applicants_table.php
+++ database/migrations/2025_10_28_131926_create_applicants_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('applicants', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('job_id');
+            $table->integer('job_id');
             $table->string('name');
             $table->string('email');
             $table->string('phone')->nullable();
