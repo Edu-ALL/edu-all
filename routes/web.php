b96c75746eeb0d040557476360e60f413aeba493
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2026-03-05 12:10:23 +0700

    optimization semrush

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::redirect('/', '/id-en', 301);
+Route::redirect('/array/pkv-games', '/id-en', 301);
+Route::redirect('/array/dominoqq', '/id-en', 301);
+Route::redirect('/array/bandarqq', '/id-en', 301);
 
 // SEO Optimization 
 Route::redirect('/id-en/contact-us/', '/id-en', 301);
